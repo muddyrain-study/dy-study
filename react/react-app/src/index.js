4c4b18dd784e8326e65041305fa282839e2e2d4a
“muddyrain” <qiu3291002845@gmail.com>
2023-03-02 17:01:56 +0800

    feat: react - form组件-context

--- react/react-app/src/index.js
+++ react/react-app/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Test from "./components/banner/test";
+import Test from "./components/form/test";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<Test />);
