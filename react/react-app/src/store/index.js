86c4716f44fbd12fcbc53a0b3d7d9b35a9e2f744
‘muddyrain’ <qiu3291002845@gmail.com>
2023-03-09 22:39:58 +0800

    feat: 理解 redux 中间件

--- react/react-app/src/store/index.js
+++ react/react-app/src/store/index.js
@@ -1,8 +1,29 @@
-import { createStore, bindActionCreators } from "../redux";
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import { bindActionCreators } from "../redux";
 import reducer from "./reducer";
 import * as userActions from "./action/user-action";
 // import { v4 as uuid } from "uuid";
-const store = createStore(reducer);
+const logger1 = (store) => (next) => (action) => {
+  console.log("中间件1");
+  console.log("旧数据", store.getState());
+  console.log("action", action);
+  next(action);
+  console.log("新数据", store.getState());
+  console.log("");
+};
+const logger2 = (store) => (next) => (action) => {
+  console.log("中间件2");
+  console.log("旧数据", store.getState());
+  console.log("action", action);
+  next(action);
+  console.log("新数据", store.getState());
+  console.log("");
+};
+//应用中间件，方式1：
+// const store = createStore(reducer, applyMiddleware(logger1, logger2));
+
+//方式2：
+const store = applyMiddleware(logger1, logger2)(createStore)(reducer);
 
 const unListen = store.subscribe(() => {
   console.log("监听器1", store.getState());
