6092003939ff672cb4a73397df11e2307c9ff9c8
“muddyrain” <qiu3291002845@gmail.com>
2023-03-09 11:15:29 +0800

    feat: react - redux action

--- react/react-app/src/redux/index.js
+++ react/react-app/src/redux/index.js
@@ -1,4 +1,6 @@
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, bindActionCreators } from "redux";
+import { DECREASE, INCREASE, SET } from "./action/action-type";
+import * as numberActions from "./action/number-action";
 /**
  * reducer本质上就是一个普通函数
  * @param state 之前仓库中的状态（数据）
@@ -6,18 +8,23 @@ import { legacy_createStore as createStore } from "redux";
  */
 function reducer(state, action) {
   //返回一个新的状态
-  if (action.type === "increase") {
+  if (action.type === INCREASE) {
     return state + 1;
-  } else if (action.type === "decrease") {
+  } else if (action.type === DECREASE) {
     return state - 1;
+  } else if (action.type === SET) {
+    return action.payload;
   }
   return state;
 }
-
 const store = createStore(reducer, 10);
-store.dispatch({
-  type: "increase",
-});
+
+//第一个参数，是action创建函数合并的对象，第二个参数是仓库的dispatch函数
+//得到一个新的对象，新对象中的属性名与第一个参数的属性名一致
+const boundActions = bindActionCreators(numberActions, store.dispatch);
+boundActions.getIncreaseAction();
+
+boundActions.getSetAction(888);
 console.log(store.getState()); //得到仓库中当前的数据
 
 window.store = store;
