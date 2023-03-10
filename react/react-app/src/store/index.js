835436d2c5243950ff87cd521fb5a4b48604f5eb
“muddyrain” <qiu3291002845@gmail.com>
2023-03-10 14:02:24 +0800

    feat: 手写 redux-thunk

--- react/react-app/src/store/index.js
+++ react/react-app/src/store/index.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore } from "../redux";
 import reducer from "./reducer";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
+import thunk from "../redux-thunk";
 //应用中间件，方式1：
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
