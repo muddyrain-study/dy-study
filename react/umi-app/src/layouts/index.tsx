eabbe68ae414f73a314afef8b9b6005addf70393
“muddyrain” <qiu3291002845@gmail.com>
2023-03-13 14:56:46 +0800

    feat: umijs

---
+++ react/umi-app/src/layouts/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.less';
+
+export default function Layout(props: any) {
+  console.log(props);
+  return (
+    <div>
+      <div>页头</div>
+      <div>页脚</div>
+    </div>
+  );
+}
---
+++ react/umi-app/src/pages/sub/index.tsx
@@ -0,0 +1,8 @@
+export default function IndexPage(props: any) {
+  console.log(props);
+  return (
+    <div>
+      <h1>sub</h1>
+    </div>
+  );
+}
